d2876de6ad214b6518635d370d07444e80059a84
Christian Tiedemann <CH.Tiedemann@gmx.de>
2022-08-10 22:25:11 +0200

    Remove old file structure

--- src/composables/useRemoteFavicon.js
+++
@@ -1,5 +0,0 @@
-import axios from 'axios';
-
-export function useRemoteFavicon(url) {
-  axios.get(url).then(console.log, console.warn);
-}
--- src/composables/useWidgetState.js
+++
@@ -1,9 +0,0 @@
-import { useStorage } from '@vueuse/core';
-
-const widgetState = useStorage('cnt-widgetState', {
-  active: undefined,
-});
-
-export function useWidgetState() {
-  return widgetState;
-}
