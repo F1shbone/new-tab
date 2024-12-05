6fa6703d17f4cf720cdbb83c23b24a60e1e29be5
Christian Tiedemann <CH.Tiedemann@gmx.de>
2024-12-05 23:19:24 +0100

    Setup Vue3 Project

---
+++ packages/newTab/src/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+//
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <p>You did it!</p>
+    </div>
+    <h1 class="text-3xl font-bold text-red-500 underline">Hello world!</h1>
+  </header>
+
+  <main>
+    <!--  -->
+  </main>
+</template>
+
+<style scoped>
+/*  */
+</style>
