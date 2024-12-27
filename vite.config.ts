6921eb9221ea10161595b405a7066f504bc5fd73
Christian Tiedemann <CH.Tiedemann@gmx.de>
2024-12-27 01:14:06 +0100

    Cleanup

--- vite.config.ts
+++ vite.config.ts
@@ -6,21 +6,20 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  /* @ts-expect-error vueDevTools_no_type */
   plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
       // Bugfix for wrong default export of delaunator in trianglify.js
       'trianglify/colorbrewer': fileURLToPath(
-        new URL('../../node_modules/trianglify/src/utils/colorbrewer.js', import.meta.url),
+        new URL('node_modules/trianglify/src/utils/colorbrewer.js', import.meta.url),
       ),
       trianglify: fileURLToPath(
-        new URL('../../node_modules/trianglify/dist/trianglify.bundle.js', import.meta.url),
+        new URL('node_modules/trianglify/dist/trianglify.bundle.js', import.meta.url),
       ),
     },
   },
   build: {
-    outDir: '../extension',
+    outDir: './extension',
   },
 })
