1dbe4f019437a44cd600f2cf0c499d917c4e02a2
F1shbone <DJ_Side@t-online.de>
2025-01-07 09:47:40 +0100

    Small screen update

--- src/App.vue
+++ src/App.vue
@@ -12,24 +12,53 @@ import TWarhammer from './components/widgets/TWarhammer.vue'
 
 <template>
   <main
-    class="overscroll-contain overflow-y-auto w-screen h-screen grid xl:grid-cols-3 xl:gap-10 2xl:grid-cols-5 grid-rows-[7rem_1fr] 2xl:gap-4 p-4"
+    class="container__base overscroll-contain overflow-y-auto w-screen h-screen grid grid-cols-3 grid-rows-[7rem_1fr] gap-4 p-4"
   >
-    <TClock class="xl:col-start-2 2xl:col-start-4" />
+    <TClock class="container__clock col-start-2" />
 
-    <div class="flex flex-col justify-between row-span-2 row-start-1 gap-4 2xl:col-span-2">
+    <div class="flex flex-col justify-between row-span-2 row-start-1 gap-4  container__widgets">
       <TBookmarks />
 
-      <div class="flex flex-col gap-4 2xl:w-1/2">
+      <div class="flex flex-col gap-4 widgets">
         <TScryfall />
         <TAlarmClock />
         <TSettings />
       </div>
     </div>
 
-    <div class="row-span-2 row-start-1 xl:col-start-3 2xl:col-start-5">
+    <div class="container__right col-start-3 row-span-2 row-start-1 ">
       <THackernews />
     </div>
 
     <TWallpaper />
   </main>
 </template>
+
+
+<style>
+/* xl */
+@media (min-width: 1280px) and (max-resolution: 1x) {
+  .container__base {
+    @apply gap-10;
+  }
+}
+/* 2xl */
+@media (min-width: 1536px) and (max-resolution: 1x) {
+  .container__base {
+    @apply grid-cols-5;
+    @apply gap-4;
+  }
+  .container__clock {
+    @apply col-start-4;
+  }
+  .container__widgets {
+    @apply col-span-2;
+  }
+  .container__right {
+    @apply col-start-5;
+  }
+  .widgets {
+    @apply w-1/2;
+  }
+}
+</style>
